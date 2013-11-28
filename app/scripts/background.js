ec3fcc7fdd0a7473d065e90edab4e4c52bc66522
Azeem Bande-Ali <A.BandeAli@gmail.com>
2013-11-28 03:55:48 -0500

    Halfway there!
    Included jquery to ease up stuff. Right now we can capture all clicks inside of wikipedia. Now we need to maintain a switch, so we only start hijacking wikipedia when the user asks. And obviously the second half is left of actually creating the graph.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,3 +3,12 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse)
+	{
+		console.log("Request from" + sender.tab.url);
+		console.log("For link" + request.href);
+		sendResponse("worked!");
+	}
+);
